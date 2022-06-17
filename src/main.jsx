d6432426234630738096d4eb9d702fb5a8da0848
DanielCVissio <107563252+DanielCVissio@users.noreply.github.com>
2022-06-17 13:59:52 -0300

    update hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HooksApp } from './HooksApp';
+import { SimpleForm } from './02-useEffect/SimpleForm';
 import './index.css';
 
+//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+//import { HooksApp } from './HooksApp';
+//import { CounterApp } from './01-useState/CounterApp';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HooksApp  />
-  </React.StrictMode>
+  //<React.StrictMode>
+    <SimpleForm />
+  //</React.StrictMode>
 )
