2fbb3749f0c6e50d4005a2052275890e5550820f
DanielCVissio <107563252+DanielCVissio@users.noreply.github.com>
2022-06-23 13:14:45 -0300

    update 161

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
+import { BrowserRouter } from "react-router-dom";
 
 import './index.css';
 
-
+//import { TodoApp } from './08-useReducer/TodoApp';
 //import { Padre } from './07-tarea-memo/Padre';
 //import './08-useReducer/intro-reducer';
 //import { CallbackHook } from './06-memos/CallbackHook';
@@ -20,7 +21,10 @@ import './index.css';
 //import { CounterApp } from './01-useState/CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-    <TodoApp />
-  //</React.StrictMode>
+  <BrowserRouter>
+    {/*<React.StrictMode>*/}
+      <MainApp />
+    {/*</React.StrictMode>*/}
+  </BrowserRouter>
+
 )
